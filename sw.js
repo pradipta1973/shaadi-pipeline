8a430b0905a6b1687722c5c1b8380a80a72ccfc8
Pradipta <pradipta.b@peopleinteractive.in>
2026-04-27 14:55:53 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = 'pipeline-v1';
+const CACHE_NAME = 'pipeline-v32';
 const urlsToCache = ['/shaadi-pipeline/', '/shaadi-pipeline/index.html'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
